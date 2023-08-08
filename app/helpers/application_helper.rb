dde5b3a5d6531a8d74a7e0994b8844d48150b232
Trevor Vallender <trevor@tsvallender.co.uk>
2023-08-08 20:29:01 +0100

    Add session helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if logged_in?
+  end
 end
