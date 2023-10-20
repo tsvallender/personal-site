e0c12a9760bd5afaad66b0b07f0ed1d4f139b548
Trevor Vallender <trevor@tsvallender.co.uk>
2023-10-20 10:38:55 +0100

    Use default Rails healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   resources :blog_posts
   resources :microposts
   resources :tags, only: [:index, :show]
+
+  get "up" => "rails/health#show", as: :rails_health_check
 end
