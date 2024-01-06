658bc90ed75392e71619602428b342014f823c32
Trevor Vallender <trevor@tsvallender.co.uk>
2024-01-06 11:17:36 +0000

    Show related microposts on learning goals

--- app/models/learning_goal.rb
+++ app/models/learning_goal.rb
@@ -8,4 +8,10 @@ class LearningGoal < ApplicationRecord
 
   has_and_belongs_to_many :tags
   accepts_nested_attributes_for :tags
+
+  def microposts
+    microposts = Micropost.none
+    tags.each { |tag| microposts = microposts.or(tag.microposts) }
+    microposts
+  end
 end
