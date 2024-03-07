4ab60b6409a7c75217c351f6e3bd62d964757475
Trevor Vallender <trevor@tsvallender.co.uk>
2024-03-07 09:14:06 +0000

    Continue wth freshen up

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -3,5 +3,7 @@
 class HomePagesController < ApplicationController
   skip_before_action :require_login
 
-  def index; end
+  def index
+    @latest_blog_post = BlogPost.published.order(created_at: :desc).first
+  end
 end
